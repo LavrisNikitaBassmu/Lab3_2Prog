cfcccd03cbf4645b8824aec9210e3c1edb047b15
Admin <Admin@WIN-0QFN66U68EP>
2024-11-23 00:38:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[,] array = new int[7, 7];
+        int value = 1;
+
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                array[i, j] = value++;
+            }
+        }
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        RotateArray(array);
+
+        Console.WriteLine("Повернутый массив на 90 градусов вправо:");
+        PrintArray(array);
+    }
+
+    static void RotateArray(int[,] array)
+    {
+        int n = array.GetLength(0);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int temp = array[i, j];
+                array[i, j] = array[j, i];
+                array[j, i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n / 2; j++)
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, n - j - 1];
+                array[i, n - j - 1] = temp;
+            }
+        }
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        int n = array.GetLength(0);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
